3e7ce16ff5dc3d310a05771051c1653207713a94
Aditya <150531010+idityaGE@users.noreply.github.com>
2025-07-27 17:31:48 +0530

    Update merge_sort.cpp

--- 02. Sorting/merge_sort.cpp
+++ 02. Sorting/merge_sort.cpp
@@ -11,23 +11,19 @@ void merge(int arr[], int low, int mid, int high) {
   vector<int> temp;
   int left = low;
   int right = mid + 1;
-  while (left <= mid && right <= high) {
-    if (arr[left] <= arr[right]) {
-      temp.push_back(arr[left]);
-      left++;
-    } else {
-      temp.push_back(arr[right]);
-      right++;
-    }
-  }
-  while (left <= mid) {
-    temp.push_back(arr[left]);
-    left++;
-  }
-  while (right <= high) {
-    temp.push_back(arr[right]);
-    right++;
-  }
+  
+  while (left <= mid && right <= high) 
+    if (arr[left] <= arr[right]) 
+      temp.push_back(arr[left++]);
+     else
+      temp.push_back(arr[right++]);
+  
+  while (left <= mid) 
+    temp.push_back(arr[left++]);
+  
+  while (right <= high) 
+    temp.push_back(arr[right++]);
+  
   for (int i = low; i <= high; i++)
     arr[i] = temp[i - low];
 }
